cb2611c02e7e9821b2167f760c5e5001e3b9079c
isidoraoOo <135176574+isidoraoOo@users.noreply.github.com>
2024-05-09 18:59:34 -0400

    Create index.php

---
+++ C:\xampp\htdocs\prueba/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <title> Home </title>
+</head>    
+<body>
+    <h1> :o </h1>
+    <button onclick="window.location.href='reserva.html'">Reservar</button>
+    <br>
+    <button onclick="window.location.href='actualizaciones.html'">Actualizar reserva</button>
+    <br>
+    <button onclick="window.location.href='eliiminar.php'">Eliminar reserva</button>
+</body>
+</html>
